9dd54bcf015af76e2407247ed57b4b7f244125fe
bakirdevedzic <devedzic.bakir2@gmail.com>
2024-03-21 01:17:17 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { HiArchiveBoxArrowDown } from "react-icons/hi2";
+
+function App() {
+  return (
+    <div className="text-3xl text-red-500">
+      <HiArchiveBoxArrowDown />
+      Test
+    </div>
+  );
+}
+
+export default App;
