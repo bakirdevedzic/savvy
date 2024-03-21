90f87a954c078ab132128a28e789e0947324a711
bakirdevedzic <devedzic.bakir2@gmail.com>
2024-03-21 15:48:49 +0100

    Configure tailwind config file

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { HiArchiveBoxArrowDown } from "react-icons/hi2";
 
 function App() {
   return (
-    <div className="text-3xl text-red-500">
+    <div className="text-3xl text-slate-700 font-poppins font-bold ">
       <HiArchiveBoxArrowDown />
       Test
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto Mono", "monospace"],
+      },
+      colors: {
+        "primary-blue": "#3248F2",
+        "primary-black": "#171215",
+        "primary-gray": "#666666",
+        "primary-white-2": "#f6f8fa",
+      },
+    },
   },
   plugins: [],
 };
