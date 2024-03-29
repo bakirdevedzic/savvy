3760564116b129d31fabf3312aaaff6936c9a2dc
bakirdevedzic <devedzic.bakir2@gmail.com>
2024-03-29 09:16:58 +0100

    Make structure od Budget page

--- src/pages/Budget.jsx
+++ src/pages/Budget.jsx
@@ -1,5 +1,33 @@
+import { MdAddBox } from "react-icons/md";
+import Button from "../ui/Button";
+import InfoCard from "../ui/InfoCard";
+
+import { useState } from "react";
+import Modal from "../ui/Modal";
+
 function Budget() {
-  return <div className="max-w-[1700px] w-[100%]">Budget</div>;
+  const [showModal, setShowModal] = useState(false);
+  const handleOnClose = () => {
+    setShowModal(false);
+  };
+  return (
+    <div className="max-w-[1700px] w-[100%] grid-rows-[150px_1fr_1fr] grid">
+      <div>
+        <InfoCard label="Total amount" amount="456845" />
+        <InfoCard />
+        <InfoCard />
+        <InfoCard />
+      </div>
+      <div>
+        <Button type="base" onClick={() => setShowModal(true)}>
+          <MdAddBox />
+          Add new
+        </Button>
+      </div>
+      <div>Tabela</div>
+      <Modal visible={showModal} onClose={handleOnClose} />
+    </div>
+  );
 }
 
 export default Budget;
---
+++ src/ui/InfoCard.jsx
@@ -0,0 +1,10 @@
+function InfoCard1({ label, amount }) {
+  return (
+    <div className="">
+      <div>{label}</div>
+      <div>${amount}</div>
+    </div>
+  );
+}
+
+export default InfoCard1;
