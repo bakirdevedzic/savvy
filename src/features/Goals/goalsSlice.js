dbe6b99b2b157fa11f6a118ac99b6a79fd1570f0
bakirdevedzic <devedzic.bakir2@gmail.com>
2024-04-13 18:06:03 +0200

    Implement goalsSlice

--- src/features/Goals/goalsSlice.js
+++ src/features/Goals/goalsSlice.js
@@ -1,5 +1,11 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { fetchGoals } from "../../services/apiGoals";
+import {
+  addGoal,
+  deleteGoal,
+  editGoal,
+  fetchGoals,
+} from "../../services/apiGoals";
+import toast from "react-hot-toast";
 
 // Async Thunks for Goals
 export const fetchGoalsAsync = createAsyncThunk(
@@ -10,29 +16,29 @@ export const fetchGoalsAsync = createAsyncThunk(
   }
 );
 
-// export const addGoalAsync = createAsyncThunk(
-//   'goals/addGoal',
-//   async (newGoal, { rejectWithValue }) => {
-//     try {
-//       const response = await addGoal(newGoal); // API call to add a new goal
-//       return response;
-//     } catch (error) {
-//       return rejectWithValue(error.message);
-//     }
-//   }
-// );
+export const addGoalAsync = createAsyncThunk(
+  "goals/addGoal",
+  async (newGoal) => {
+    const response = await addGoal(newGoal); // API call to add a new goal
+    return response;
+  }
+);
 
-// export const editGoalAsync = createAsyncThunk(
-//   'goals/editGoal',
-//   async (editedGoal, { rejectWithValue }) => {
-//     try {
-//       const response = await editGoalApi(editedGoal); // API call to edit a goal
-//       return response;
-//     } catch (error) {
-//       return rejectWithValue(error.message);
-//     }
-//   }
-// );
+export const editGoalAsync = createAsyncThunk(
+  "goals/editGoal",
+  async (editedGoal) => {
+    const response = await editGoal(editedGoal); // API call to edit a goal
+    return response;
+  }
+);
+
+export const deleteGoalAsync = createAsyncThunk(
+  "goals/deleteGoal",
+  async (goalId) => {
+    const response = await deleteGoal(goalId);
+    return response;
+  }
+);
 
 const initialState = {
   activeGoals: [],
@@ -63,34 +69,49 @@ const goalsSlice = createSlice({
       .addCase(fetchGoalsAsync.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.error.message;
+      })
+      .addCase(addGoalAsync.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(addGoalAsync.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.goals.push(action.payload);
+        toast.success("Goal added successfully");
+      })
+      .addCase(addGoalAsync.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      })
+      .addCase(editGoalAsync.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(editGoalAsync.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        const editedIndex = state.goals.findIndex(
+          (g) => g.id === action.payload.id
+        );
+        if (editedIndex !== -1) {
+          state.goals[editedIndex] = action.payload;
+        }
+        toast.success("Goal edited successfully");
+      })
+      .addCase(editGoalAsync.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      })
+      .addCase(deleteGoalAsync.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(deleteGoalAsync.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        toast.success("Goal deleted successfully");
+        state.goals = state.goals.filter((goal) => goal.id !== action.payload);
+      })
+      .addCase(deleteGoalAsync.rejected, (state, action) => {
+        state.status = "failed";
+        toast.error(action.error.message);
+        state.error = action.error.message;
       });
-    //   .addCase(addGoalAsync.pending, (state) => {
-    //     state.status = "loading";
-    //   })
-    //   .addCase(addGoalAsync.fulfilled, (state, action) => {
-    //     state.status = "succeeded";
-    //     state.goals.push(action.payload);
-    //   })
-    //   .addCase(addGoalAsync.rejected, (state, action) => {
-    //     state.status = "failed";
-    //     state.error = action.payload;
-    //   })
-    //   .addCase(editGoalAsync.pending, (state) => {
-    //     state.status = "loading";
-    //   })
-    //   .addCase(editGoalAsync.fulfilled, (state, action) => {
-    //     state.status = "succeeded";
-    //     const editedIndex = state.goals.findIndex(
-    //       (g) => g.id === action.payload.id
-    //     );
-    //     if (editedIndex !== -1) {
-    //       state.goals[editedIndex] = action.payload;
-    //     }
-    //   })
-    //   .addCase(editGoalAsync.rejected, (state, action) => {
-    //     state.status = "failed";
-    //     state.error = action.payload;
-    //   });
   },
 });
 
